77680b32907e389161fd066133235cf3b96bc6eb
marwenfray <fraymarwen@gmail.com>
2020-12-20 14:48:23 +0100

    fixing bugs

--- client/src/js/reducer/index.js
+++ client/src/js/reducer/index.js
@@ -5,13 +5,12 @@ import {combineReducers} from "redux"
 import { adminReducer } from "./adminReducer"
 import  authReducer from './authReducer'
 import { doctorReducer } from "./doctorReducer"
-import {alertReducer} from "./alertReducer"
 
 const persistConfig = {
     key: 'root',
     storage,
   }
 const rootReducer = combineReducers ({
-    authReducer,doctorReducer,adminReducer,alertReducer
+    authReducer,doctorReducer,adminReducer
 })
 export default persistReducer(persistConfig, rootReducer);
